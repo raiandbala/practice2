5cc1ace458b96c6315a67ade924f9739a3b87f72
Shubham <raiandbala@outlook.com>
2022-06-07 15:04:47 +0530

    hotfix commit 4

--- src/test.java
+++ src/test.java
@@ -12,27 +12,24 @@ public class test{
 	public static void main(String[]args) {
 		new1 obj = new new1();
 		obj.add(4, 7);
-//<<<<<<< HEAD
+
 		for(int i=1; i<=3; i++) {
 			System.out.println('*');
 		}
-//=======
+
 		System.out.println("shubham");
-//<<<<<<< HEAD
+
 		System.out.println("welcome");
 		System.out.println("visit again");
 		System.out.println("Jay Shree Ram");
 		System.out.println("Jay Shree Ram");
 		System.out.println("shubham");
 		System.out.println("shubham");
-<<<<<<< HEAD
-//>>>>>>> refs/heads/Integration_branch
-=======
-//=======
+
 		System.out.println("raj");
 		System.out.println("prays Shree Ramji");
 		System.out.println("prays Shree Hanumanji");
-//>>>>>>> refs/heads/feature1
->>>>>>> refs/heads/Integration_branch
+		System.out.println("prays Shree Mata Pitaji");
+
 	}
 }

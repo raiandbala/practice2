922c9eb85ed4024138b416dbaf38a484a84cc07a
Shubham <raiandbala@outlook.com>
2022-06-07 13:20:11 +0530

    production branch commit1

---
+++ src/test.java
@@ -0,0 +1,16 @@
+class new1{
+	int a;
+	int b;
+	public void add(int i, int j) {
+		a =i;
+		b=j;
+		int c = a+b;
+		System.out.println("addition is :"+c);
+	}	
+}
+public class test{
+	public static void main(String[]args) {
+		new1 obj = new new1();
+		obj.add(4, 7);
+	}
+}
